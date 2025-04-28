4975f4caeb8361e4f5e2eafa192c33a06f4007b4
L3gvccy <oleksandr_iv@knu.ua>
2025-04-28 19:27:08 +0300

    Add teacher app to installed apps and implement code generation function

--- learning_plat/teacher/views.py
+++ learning_plat/teacher/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
+import string
+import random
 
 # Create your views here.
 def generate_code():
-    pass
+    characters = string.ascii_uppercase + string.digits
+    code = ''.join(random.choices(characters, k=6))
+    return code
 
 def create_course(request):
     pass
