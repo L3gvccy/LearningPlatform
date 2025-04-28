648bc024b1239fce71521a4225990ab3642e0f3d
Avokad <froggeribbit@gmail.com>
2025-04-28 19:29:21 +0300

    Model for singular course

--- learning_plat/teacher/models.py
+++ learning_plat/teacher/models.py
@@ -1,3 +1,16 @@
 from djongo import models
+from accounts.models import User
 
 # Create your models here.
+
+class Course(models.Model):
+    courseId = models.CharField(max_length=6,primary_key=True)
+    title = models.CharField(max_length=128)
+    desc = models.CharField(max_length=256)
+    teacher = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='teacher'
+    )
+    IsArchived = models.BooleanField()
+
