c49b632ef90dabf7718b42221416fcd1bb2a516e
L3gvccy <oleksandr_iv@knu.ua>
2025-04-28 19:17:07 +0300

    Initialize teacher app with basic structure and placeholder files

---
+++ learning_plat/teacher/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from teacher.views import a
+
+urlpatterns = [
+    
+]
---
+++ learning_plat/teacher/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def generate_code():
+    pass
+
+def create_course(request):
+    pass
