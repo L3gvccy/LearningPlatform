99ffb45353d394882db71f77373f22f69cf6f15f
L3gvccy <oleksandr_iv@knu.ua>
2025-04-30 20:50:20 +0300

    Add student management features to course view and update lesson creation redirect

--- learning_plat/teacher/urls.py
+++ learning_plat/teacher/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from teacher.views import create_course, display_courses
-from course.views import view_course, edit_course
+from course.views import view_course, edit_course, remove_student
 from lesson.views import create_lesson
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('courses/<str:courseId>/', view_course, name='view_course_teacher'),
     path('courses/edit/<str:courseId>/', edit_course, name='edit_course'),
     path('courses/<str:courseId>/create_lesson/', create_lesson, name='create_lesson'),
+    path('courses/<str:courseId>/remove_student/<int:studentId>', remove_student, name='remove_student'),
 ]
