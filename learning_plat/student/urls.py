c89bdb9a3874a60685b93324ca73ec7c542f8035
L3gvccy <oleksandr_iv@knu.ua>
2025-04-30 13:20:10 +0300

    Add view course functionality for students and teachers, including URL routing and template integration

--- learning_plat/student/urls.py
+++ learning_plat/student/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from student.views import connect_to_course, view_my_courses, leave_course
+from student.views import connect_to_course, view_my_courses, leave_course, view_course
 
 urlpatterns = [
     path('connect_course/', connect_to_course, name='connect_to_course'),
     path('my_courses/', view_my_courses, name='view_my_courses'),
     path('leave_course/<str:courseId>/', leave_course, name='leave_course'),
+    path('course/<str:courseId>/', view_course, name='view_course_student')
 ]
--- learning_plat/teacher/urls.py
+++ learning_plat/teacher/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from teacher.views import create_course, display_courses
+from teacher.views import create_course, display_courses, view_course
 
 urlpatterns = [
     path('create_course/', create_course, name='create_course'),
-    path('courses/', display_courses, name='display_courses')
+    path('courses/', display_courses, name='display_courses'),
+    path('course/<str:courseId>/', view_course, name='view_course_teacher')
 ]
