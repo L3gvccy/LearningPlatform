767fa85412c43077200d0220d87dc722febb1a05
L3gvccy <oleksandr_iv@knu.ua>
2025-04-30 21:30:20 +0300

    Add archive functionality for courses with toggle button in course view

--- learning_plat/teacher/urls.py
+++ learning_plat/teacher/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from teacher.views import create_course, display_courses
-from course.views import view_course, edit_course, remove_student
+from course.views import view_course, edit_course, remove_student, archive_course
 from lesson.views import create_lesson
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('courses/', display_courses, name='display_courses'),
     path('courses/<str:courseId>/', view_course, name='view_course_teacher'),
     path('courses/edit/<str:courseId>/', edit_course, name='edit_course'),
+    path('courses/archive/<str:courseId>/', archive_course, name='archive_course'),
     path('courses/<str:courseId>/create_lesson/', create_lesson, name='create_lesson'),
     path('courses/<str:courseId>/remove_student/<int:studentId>', remove_student, name='remove_student'),
 ]
