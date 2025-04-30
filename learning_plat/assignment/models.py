83d0d8bb8198afc75505e9300516159cebfacec8
ArsenAltunyan-alt <arsenaltunyan@knu.ua>
2025-04-30 22:23:03 +0300

    Add assignment feature with model, views, templates, and URL routing

---
+++ learning_plat/assignment/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from lesson.models import Lesson
+
+class Assignment(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=512)
+    due_date = models.DateTimeField()
+    lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        related_name='assignment_lesson'
+    )
--- learning_plat/teacher/urls.py
+++ learning_plat/teacher/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from teacher.views import create_course, display_courses
 from course.views import view_course, edit_course, remove_student
 from lesson.views import create_lesson
+from assignment.views import create_assignment
 
 urlpatterns = [
     path('create_course/', create_course, name='create_course'),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('courses/edit/<str:courseId>/', edit_course, name='edit_course'),
     path('courses/<str:courseId>/create_lesson/', create_lesson, name='create_lesson'),
     path('courses/<str:courseId>/remove_student/<int:studentId>', remove_student, name='remove_student'),
+    path('courses/<str:courseId>/<str:lessonId>/create_assignment/', create_assignment, name='create_assignment'),
 ]
