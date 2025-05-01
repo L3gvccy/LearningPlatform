47b3fa3cc0429b706cb65b78f26e06839614fcd9
L3gvccy <oleksandr_iv@knu.ua>
2025-05-01 23:46:57 +0300

    Add assignment listing and submission status to course view

--- learning_plat/course/views.py
+++ learning_plat/course/views.py
@@ -3,17 +3,30 @@ from django.contrib import messages
 from teacher.models import Course
 from lesson.models import Lesson
 from accounts.models import Student, User
+from assignment.models import Assignment
+from submission.models import Submission
 
 # Create your views here.
 def view_course(request, courseId):
     course = Course.objects.get(courseId = courseId)
     lessons = Lesson.objects.filter(course_id = course).order_by('-createdAt')
     students = Student.objects.all().filter(courses_id__contains = courseId)
+    assignments = Assignment.objects.filter(lesson__course = course).order_by('-lesson__createdAt')
+    submissions = None
+    submitted_ids = None
+    try:
+        student = Student.objects.get(user = request.user)
+        submissions = Submission.objects.filter(student=student)
+        submitted_ids = set(submissions.values_list('assignment_id', flat=True))
+    except:
+        pass
 
     context = {
         'course': course,
         'lessons' : lessons,
-        'students' : students
+        'students' : students,
+        'assignments': assignments,
+        'submitted_ids': submitted_ids
     }
 
     return render(request, 'course/view_course.html', context)
